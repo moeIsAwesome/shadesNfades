349afd5f1f7e91b5ae7ebaa77b1967a5c006c82d
moeIsAwesome <musavimoe@gmail.com>
2022-02-07 18:05:50 +0330

    First Commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
+
+import Values from 'values.js';
+
+function App() {
+  const [color, setColor] = useState('#00b7e7');
+  const [error, setError] = useState(false);
+  const [percent, setPercent] = useState(10);
+  const [list, setList] = useState(new Values('#00b7e7').all(10));
+  const [alert, setAlert] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    try {
+      let colors = new Values(color).all(parseInt(percent));
+      setList(colors);
+      setError(false);
+    } catch (error) {
+      setError(true);
+    }
+  };
+  return (
+    <div className="glass-panel">
+      <section className="glass-panel2">
+        <h1 className="title">The shades and fades of life</h1>
+        <form onSubmit={handleSubmit}>
+          <button className="glass-button" type="submit">
+            Submit
+          </button>
+          <input
+            type="text"
+            className={`${error ? 'error' : null}`}
+            value={color}
+            placeholder="#f15025"
+            onChange={(e) => {
+              setColor(e.target.value);
+            }}
+          />
+
+          <label className="input-label">
+            /
+            <input
+              className="percent"
+              type="number"
+              min={0}
+              value={percent}
+              onChange={(e) => {
+                setPercent(e.target.value);
+              }}
+            />
+          </label>
+        </form>
+      </section>
+      <section className="glass-panel3">
+        <div className="colors">
+          {list.map((color, index) => {
+            return (
+              <SingleColor
+                key={index}
+                {...color}
+                index={index}
+                hexColor={color.hex}
+                list={list}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SingleColor.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import rgbToHex from './utils';
+import { IoCopyOutline } from 'react-icons/io5';
+
+const SingleColor = ({ rgb, weight, index, hexColor, list }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 1700);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > list.length / 2 && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      {alert ? (
+        <p className="copied">
+          COPIED <IoCopyOutline className="icon" />
+        </p>
+      ) : (
+        <>
+          <p className="percent-value">{weight}%</p>{' '}
+          <p className="color-value">{hexValue}</p>
+        </>
+      )}
+    </article>
+  );
+};
+
+export default SingleColor;
